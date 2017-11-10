8c0d3602e9247ad0b5c9209fd3a8d8d8e87e47ba
kbrey <katie.brey@gmail.com>
2017-11-09 20:55:18 -0500

    Adding clustering-then-svm code

--- experiment.py
+++ experiment.py
@@ -14,26 +14,14 @@ from model import PaperData
 from utility import study
 from results import results_process
 import numpy as np
-#import wget
+import wget
 import zipfile
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import neighbors
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cluster import KMeans
 
-
-def tune_learner(learner, train_X, train_Y, tune_X, tune_Y, goal,
-                 target_class=None):
-  """
-  :param learner:
-  :param train_X:
-  :param train_Y:
-  :param tune_X:
-  :param tune_Y:
-  :param goal:
-  :param target_class:
-  :return:
-  """
+def tune_learner(learner, train_X, train_Y, tune_X, tune_Y, goal, target_class=None):
   if not target_class:
     target_class = goal
   clf = learner(train_X, train_Y, tune_X, tune_Y, goal)
@@ -72,17 +60,15 @@ def get_acc(cm):
   return out
 
 
-@study
-def run_tuning_SVM(word2vec_src, repeats=1,
-                   fold=10,
-                   tuning=True):
-  """
+"""
   :param word2vec_src:str, path of word2vec model
   :param repeats:int, number of repeats
   :param fold: int,number of folds
   :param tuning: boolean, tuning or not.
   :return: None
   """
+@study
+def run_tuning_SVM(word2vec_src, repeats=1, fold=10, tuning=True):
   print("# word2vec:", word2vec_src)
   word2vec_model = gensim.models.Word2Vec.load(word2vec_src)
   data = PaperData(word2vec=word2vec_model)
@@ -118,27 +104,53 @@ def run_tuning_SVM(word2vec_src, repeats=1,
   print_results(clfs)
 
 
-@study
-def run_SVM(word2vec_src):
-  """
+"""
   Run SVM+word embedding experiment !
   This is the baseline method.
   :return:None
   """
+@study
+def run_SVM(word2vec_src, train_pd):
   print("# word2vec:", word2vec_src)
-  #clf = svm.SVC(kernel="rbf", gamma=0.005)
-  #clf = neighbors.KNeighborsClassifier(n_neighbors = 4)
-  clf = KMeans(n_clusters=4, init='k-means++', max_iter=100, n_init=1)
-  word2vec_model = gensim.models.Word2Vec.load(word2vec_src)
+  clf = svm.SVC(kernel="rbf", gamma=0.005)
   word2vec_model = gensim.models.Word2Vec.load(word2vec_src)
   data = PaperData(word2vec=word2vec_model)
-  train_pd = load_vec(data, data.train_data, use_pkl=False)
+  if train_pd is None: train_pd = load_vec(data, data.train_data, use_pkl=False)
   test_pd = load_vec(data, data.test_data, use_pkl=False)
   train_X = train_pd.loc[:, "Output"].tolist()
   train_Y = train_pd.loc[:, "LinkTypeId"].tolist()
   test_X = test_pd.loc[:, "Output"].tolist()
   test_Y = test_pd.loc[:, "LinkTypeId"].tolist()
   clf.fit(train_X, train_Y)
+  results_SVM(clf, train_X, train_Y, test_X, test_Y)
+
+
+def results_SVM(clf, train_X, train_Y, test_X, test_Y):
+  predicted = clf.predict(test_X)
+  # labels: ["Duplicates", "DirectLink","IndirectLink", "Isolated"]
+  print(metrics.classification_report(test_Y, predicted, labels=["1", "2", "3", "4"], digits=3))
+  #cm=metrics.confusion_matrix(test_Y, predicted, labels=["1", "2", "3", "4"])
+  #print("accuracy  ", get_acc(cm))
+
+def run_kmeans(word2vec_src):
+  numClusters = 4
+  print("# word2vec:", word2vec_src)
+  clf = KMeans(n_clusters=numClusters, init='k-means++', max_iter=100, n_init=1)
+  word2vec_model = gensim.models.Word2Vec.load(word2vec_src)
+  data = PaperData(word2vec=word2vec_model)
+  train_pd = load_vec(data, data.train_data, use_pkl=False)
+  train_X = train_pd.loc[:, "Output"].tolist()
+  clf.fit(train_X)
+
+  data.train_data['clabel'] = clf.labels_
+  for l in range(numClusters):
+    print("Label " + str(l))
+    cluster = data.train_data.loc[data.train_data['clabel'] == l]
+    run_SVM(word2vec_src, cluster)
+  return clf
+
+# Not used, but wanted to put this code somewhere
+def results_kmeans(clf, train_X, train_Y, test_X, test_Y):
   predicted = clf.predict(test_X)
   print("Homogeneity: %0.3f" % metrics.homogeneity_score(train_Y, clf.labels_))
   print("Completeness: %0.3f" % metrics.completeness_score(train_Y, clf.labels_))
@@ -147,15 +159,6 @@ def run_SVM(word2vec_src):
         % metrics.adjusted_rand_score(train_Y, clf.labels_))
   print("Silhouette Coefficient: %0.3f"
         % metrics.silhouette_score(train_X, clf.labels_, sample_size=1000))
-  # print(metrics.classification_report(test_Y, predicted,
-  #                                     labels=["1", "2", "3", "4"],
-  #                                     # target_names=["Duplicates", "DirectLink",
-  #                                     #               "IndirectLink",
-  #                                     #               "Isolated"],
-  #                                     digits=3))
-  # cm=metrics.confusion_matrix(test_Y, predicted, labels=["1", "2", "3", "4"])
-  # print("accuracy  ", get_acc(cm))
-
 
 def prepare_word2vec():
   print("Downloading pretrained word2vec models")
@@ -164,7 +167,7 @@ def prepare_word2vec():
   with zipfile.ZipFile(file_name, "r") as zip_ref:
     zip_ref.extractall()
 
-
+########################## MAIN ##########################
 if __name__ == "__main__":
   word_src = "word2vecs_models"
   if not os.path.exists(word_src):
@@ -172,11 +175,14 @@ if __name__ == "__main__":
   elif len(os.listdir(word_src)) == 0:
     os.rmdir(word_src)
     prepare_word2vec()
-  for x in range(10):
+  for x in range(1):
     random.seed(x)
     np.random.seed(x)
     myword2vecs = [os.path.join(word_src, i) for i in os.listdir(word_src)
                    if "syn" not in i]
-    run_SVM(myword2vecs[x])
-    print("Run completed--------------------------------------------------")
+    
+    print("\n########### Plain SVM ###########")
+    run_SVM(myword2vecs[x], None)
     #run_tuning_SVM(myword2vecs[x])
+    print("\n########### SVM with kmeans ###########")
+    kmeans = run_kmeans(myword2vecs[x])
