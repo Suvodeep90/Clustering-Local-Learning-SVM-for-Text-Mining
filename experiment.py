0e9b00f02fcd61658550781f24b55a326685adee
Suvodeep90 <31140098+Suvodeep90@users.noreply.github.com>
2017-11-16 23:24:31 -0500

    Added code for Kmeans with threading

--- experiment.py
+++ experiment.py
@@ -16,19 +16,21 @@ from results import results_process
 import numpy as np
 #import wget
 import zipfile
-from sklearn.naive_bayes import MultinomialNB
 from sklearn import neighbors
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.cluster import KMeans
 from sklearn import metrics
 import threading
+from threading import Barrier
 import timeit
 import multiprocessing
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.lda import LDA
 from sklearn.decomposition import NMF, LatentDirichletAllocation
 from sklearn.neighbors import NearestNeighbors
-
+from sklearn.cluster import KMeans
+from sklearn.cluster import AffinityPropagation
+import collections
+from multiprocessing import Queue
 
 def tune_learner(learner, train_X, train_Y, tune_X, tune_Y, goal,
                  target_class=None):
@@ -202,7 +204,7 @@ def run_tuning_LDA(word2vec_src, repeats=1,
   print_results(clfs)
 
 @study
-def run_SVM(word2vec_src): 
+def run_SVM_baseline(word2vec_src): 
   """
   Run SVM+word embedding experiment !
   This is the baseline method.
@@ -885,7 +887,6 @@ def run_KNN_KNN(word2vec_src):
 #  clf3.fit(classX2,classY2)
 #  clf4.fit(classX3,classY3)
 #  clf5.fit(classX4,classY4)
-  stop = timeit.default_timer()
   predicted0 = clf.predict(test_X)
   for i in range(len(predicted0)):
       if predicted0[i] == '1':
@@ -953,36 +954,6 @@ def run_KNN_KNN(word2vec_src):
   print("Total Model training time: ", (stop1 - start0))
 
 
-
-@study
-def run_KMeans(word2vec_src):
-  """
-  Run KMeans+word embedding experiment !
-  This is the baseline method.
-  :return:None
-  """
-  # Create a subplot with 1 row and 2 columns
-  print("# word2vec:", word2vec_src)
-  clf = KMeans(n_clusters=4, init='k-means++', max_iter=100, n_init=1)
-  word2vec_model = gensim.models.Word2Vec.load(word2vec_src)
-  data = PaperData(word2vec=word2vec_model)
-  train_pd = load_vec(data, data.train_data, use_pkl=False)
-  test_pd = load_vec(data, data.test_data, use_pkl=False)
-  train_X = train_pd.loc[:, "Output"].tolist()
-  train_Y = train_pd.loc[:, "LinkTypeId"].tolist()
-  test_X = test_pd.loc[:, "Output"].tolist()
-  test_Y = test_pd.loc[:, "LinkTypeId"].tolist()
-  clf.fit(train_X, train_Y)
-  predicted = clf.predict(test_X)
-  x =  list(np.asarray(clf.labels_) + 1)
-  print("Homogeneity: %0.3f" % metrics.homogeneity_score(train_Y, x))
-  print("Completeness: %0.3f" % metrics.completeness_score(train_Y, clf.labels_))
-  print("V-measure: %0.3f" % metrics.v_measure_score(train_Y, clf.labels_))
-  print("Adjusted Rand-Index: %.3f"
-        % metrics.adjusted_rand_score(train_Y, clf.labels_))
-  print("Silhouette Coefficient: %0.3f"
-        % metrics.silhouette_score(train_X, clf.labels_, sample_size=1000))
-
 @study
 def run_KMeans_Wpair(word2vec_src):
   """
@@ -1019,8 +990,218 @@ def run_KMeans_Wpair(word2vec_src):
   print("Silhouette Coefficient: %0.3f"
         % metrics.silhouette_score(train_X, clf.labels_, sample_size=1000))
 
+#################Katie's Code +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# returns the svm model
+def run_SVM(word2vec_src, train_pd, queue):
+  print("# word2vec:", word2vec_src)
+  clf = svm.SVC(kernel="rbf", gamma=0.005)
+#  word2vec_model = gensim.models.Word2Vec.load(word2vec_src)
+#  data = PaperData(word2vec=word2vec_model)
+#  print("Train data: " + str(train_pd.shape))
+#  if train_pd is None: train_pd = load_vec(
+#      data, data.train_data, use_pkl=False)
+  train_X = train_pd.loc[:, "Output"].tolist()
+  train_Y = train_pd.loc[:, "LinkTypeId"].tolist()
+  start = timeit.default_timer()
+  clf.fit(train_X, train_Y)
+  stop = timeit.default_timer()
+  print("SVM Model Train Time", (stop-start))
+  queue.put(clf)
+  return clf
+
+# parses and returns a given svm in the format of dictionary -
+# [class](precision, recall, f1score, support)
+def results_SVM(clf, test_X, test_Y):  
+  predicted = clf.predict(test_X)
+  # labels: ["Duplicates", "DirectLink","IndirectLink", "Isolated"]
+  report_gen = metrics.classification_report(
+      test_Y, predicted, labels=["1", "2", "3", "4"], digits=3)
+  parsed_report = parse_classification_report(report_gen)
+  return parsed_report
+ #cm=metrics.confusion_matrix(test_Y, predicted, labels=["1", "2", "3", "4"])
+  #print("accuracy  ", get_acc(cm)
 
 
+def total_summary(result_set, num_rows, start0,start1,stop0,stop1):
+  weightedAvgs = [0, 0, 0]
+  for l in result_set:
+    avg_list = l['avg']
+    for i in range(3):
+      support_count = avg_list[3]
+      weightedAvgs[i] += (avg_list[i] * support_count)/num_rows 
+
+  result = {}
+  result['precision'] = weightedAvgs[0]
+  result['recall'] = weightedAvgs[1]
+  result['f1'] = weightedAvgs[2]
+  print(result)
+  print("1st Model training time: ", (stop0 - start0))
+  print("layer 2 Models training time: ", (stop1 - start1))
+  print("Total Model training time: ", (stop1 - start0))
+  
+def run_kmeans(word2vec_src):
+
+  print("# word2vec:", word2vec_src)
+  word2vec_model = gensim.models.Word2Vec.load(word2vec_src)
+  data = PaperData(word2vec=word2vec_model)
+  train_pd = load_vec(data, data.train_data, use_pkl=False)
+  test_pd = load_vec(data, data.test_data, use_pkl=False)
+  train_X = train_pd.loc[:, "Output"].tolist()
+  queue = Queue()
+
+  #numClusters = optimalK(train_X)
+  numClusters = 5
+  print("Found optimal k: " + str(numClusters))
+  clf = KMeans(n_clusters=numClusters,
+               init='k-means++', max_iter=200, n_init=1)
+  
+  start0 = timeit.default_timer()
+  clf.fit(train_X)
+  stop0 = timeit.default_timer()
+
+  svm_models = []  # maintain a list of svms
+  s1 = timeit.default_timer()
+  data.train_data['clabel'] = clf.labels_
+  s2 = timeit.default_timer()
+  print("Inter - ", (s2-s1))
+  start1 = timeit.default_timer()
+  b = Barrier(numClusters-1)
+  for l in range(numClusters):
+    cluster = data.train_data.loc[data.train_data['clabel'] == l]   
+    t = threading.Thread(target=run_SVM, args = [word2vec_src,cluster,queue])
+    threads.append(t)
+    t.start()
+    response = queue.get()
+    print(response)
+    svm_models.append(response)
+    #svm_models.append(run_SVM(word2vec_src, cluster))
+ # b.wait()
+  t.join()
+  stop1 = timeit.default_timer()
+
+  svm_results = [] # maintain a list of svm results
+  test_X = test_pd.loc[:, "Output"].tolist()
+  predicted = clf.predict(test_X)
+  data.test_data['clabel'] = predicted
+  for l in range(numClusters):
+    #print("Label " + str(l))
+    cluster = data.test_data.loc[data.test_data['clabel'] == l]
+    svm_model = svm_models[l]
+    cluster_X = cluster.loc[:, "Output"].tolist()
+    cluster_Y = cluster.loc[:, "LinkTypeId"].tolist()
+    svm_results.append(results_SVM(svm_model, cluster_X, cluster_Y))# store all the SVM result report in a dictionary
+
+    # call the helper method to summarize the svm results
+  total_summary(svm_results, test_pd.shape[0],start0,start1,stop0,stop1)
+
+# Source: https://anaconda.org/milesgranger/gap-statistic/notebook
+
+
+def optimalK(data, nrefs=3, maxClusters=15):
+  """
+  Calculates KMeans optimal K using Gap Statistic from Tibshirani, Walther, Hastie
+  Params:
+      data: ndarry of shape (n_samples, n_features)
+      nrefs: number of sample reference datasets to create
+      maxClusters: Maximum number of clusters to test for
+  Returns: (gaps, optimalK)
+  """
+  gaps = np.zeros((len(range(1, maxClusters)),))
+  resultsdf = pd.DataFrame({'clusterCount': [], 'gap': []})
+  for gap_index, k in enumerate(range(1, maxClusters)):
+
+    # Holder for reference dispersion results
+    refDisps = np.zeros(nrefs)
+
+      # For n references, generate random sample and perform kmeans getting resulting dispersion of each loop
+    for i in range(nrefs):
+
+      # Create new random reference set
+      # randomReference = np.random.random_sample(size=data.shape)
+
+      # Fit to it
+      km = KMeans(n_clusters=k, init='k-means++', max_iter=200, n_init=1)
+      km.fit(data)
+
+      refDisp = km.inertia_
+      refDisps[i] = refDisp
+
+      # Fit cluster to original data and create dispersion
+      km = KMeans(k)
+      km.fit(data)
+
+      origDisp = km.inertia_
+      # print(str(i+1) + ": " + str(origDisp))
+
+      # Calculate gap statistic
+      gap = np.log(np.mean(refDisps)) - np.log(origDisp)
+
+      # Assign this loop's gap statistic to gaps
+      gaps[gap_index] = gap
+
+      resultsdf = resultsdf.append(
+          {'clusterCount': k, 'gap': gap}, ignore_index=True)
+
+  # return (gaps.argmax() + 1, resultsdf)  # Plus 1 because index of 0 means 1 cluster is optimal, index 2 = 3 clusters are optimal
+  return gaps.argmax()
+
+# Not used, but wanted to put this code somewhere
+def results_kmeans(clf, train_X, train_Y, test_X, test_Y):
+  predicted = clf.predict(test_X)
+  print("Homogeneity: %0.3f" % metrics.homogeneity_score(train_Y, clf.labels_))
+  print("Completeness: %0.3f" %
+        metrics.completeness_score(train_Y, clf.labels_))
+  print("V-measure: %0.3f" % metrics.v_measure_score(train_Y, clf.labels_))
+  print("Adjusted Rand-Index: %.3f"
+        % metrics.adjusted_rand_score(train_Y, clf.labels_))
+  print("Silhouette Coefficient: %0.3f"
+        % metrics.silhouette_score(train_X, clf.labels_, sample_size=1000))
+
+"""
+Parse a sklearn classification report into a dict keyed by class name
+and containing a tuple (precision, recall, fscore, support) for each class
+Reference: https://gist.github.com/julienr/6b9b9a03bd8224db7b4f
+"""
+def parse_classification_report(clfreport):
+
+    lines = clfreport.split('\n')
+    # Remove empty lines
+    lines = list(filter(lambda l: not len(l.strip()) == 0, lines))
+
+    # Starts with a header, then score for each class and finally an average
+    header = lines[0]
+    cls_lines = lines[1:-1]
+    avg_line = lines[-1]
+
+    assert header.split() == ['precision', 'recall', 'f1-score', 'support']
+    assert avg_line.split()[0] == 'avg'
+
+    # class names can have spaces - figure the width of the class field
+    # using indentation of the precision header
+    cls_field_width = len(header) - len(header.lstrip())
+
+    # Now, collect all the class names and score in a dict
+    def parse_line(l):
+        """Parse a line of classification_report"""
+        cls_name = l[:cls_field_width].strip()
+        precision, recall, fscore, support = l[cls_field_width:].split()
+        precision = float(precision)
+        recall = float(recall)
+        fscore = float(fscore)
+        support = int(support)
+        return (cls_name, precision, recall, fscore, support)
+
+    data = collections.OrderedDict()
+    for l in cls_lines:
+        ret = parse_line(l)
+        cls_name = ret[0]
+        scores = ret[1:]
+        data[cls_name] = scores
+
+    data['avg'] = parse_line(avg_line)[1:]  # average
+    return data
+  
+#################Katie's Code +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 def prepare_word2vec():
   print("Downloading pretrained word2vec models")
   url = "https://zenodo.org/record/807727/files/word2vecs_models.zip"
@@ -1045,14 +1226,15 @@ if __name__ == "__main__":
 #    t = threading.Thread(target=run_tuning_SVM_KNN, args = [myword2vecs[x]])
 #    threads.append(t)
 #    t.start()
-    #run_SVM(myword2vecs[x])
+    #run_SVM_baseline(myword2vecs[x])
     #run_SVM_KNN_thread(myword2vecs[x])
     #run_LinearDiscriminantAnalysis(myword2vecs[x])
     #run_KNN(myword2vecs[x])
     #run_SVM_KNN(myword2vecs[x])
     #run_KMeans_Wpair(myword2vecs[x])
+    run_kmeans(myword2vecs[x])
     #run_KNN_SVM(myword2vecs[x])
-    run_KNN_KNN(myword2vecs[x])
+    #run_KNN_KNN(myword2vecs[x])
     #Srun_LDA(myword2vecs[x])
     #run_RNN(myword2vecs[x])
     #print("Run completed for baseline model--------------------------------------------------")
